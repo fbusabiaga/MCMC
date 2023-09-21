1a200505e4e26fce1275ecd8ae48e31ef8fb960b
Florencio Balboa Usabiaga <fbusabiaga@gmail.com>
2023-09-21 18:49:52 +0200

    timing code

--- examples/suspension_2d/main_fast.jl
+++ examples/suspension_2d/main_fast.jl
@@ -65,77 +65,88 @@ r_vectors = many_bodies_utils.get_r_vectors(bodies)
 r_vectors_offsets = many_bodies_utils.get_r_vectors_offsets(bodies)
 Nmarkers = r_vectors_offsets[end]
 
-# Open output files
-f_config = open(output_name * ".config", "w")
 
-# Loop steps
+# Prepare steps
 n_steps = parse(Int, options["n_steps"][1])
 n_save = parse(Int, options["n_save"][1])
 initial_step = haskey(options, "initial_step") ? parse(Int64, options["initial_step"][1]) : 0
-for step = initial_step:1:n_steps-1
+function run(bodies, r_vectors, r_vectors_offsets, Nmarkers, n_steps, n_save, initial_step, kT, dx, dtheta, radius, e0, lambda)
+
+  # Open output files
+  f_config = open(output_name * ".config", "w")
+
+  # Leaf size for tree 
+  leafsize = floor(Int, log2(Nmarkers * length(bodies)))
+
+  # Loop steps
+  for step = initial_step:1:n_steps-1
     
-  # Save config
-  if (step % n_save == 0) && (step >= 0)
-    println("step = ", step)
+    # Save config
+    if (step % n_save == 0) && (step >= 0)
+      println("step = ", step)
+      write(f_config, string(length(bodies)), "\n")
+      for b in bodies
+        println(f_config, join(b.q, " "), " ", b.orientation)
+      end
+    end
+    
+    # Build tree
+    kdtree = NearestNeighbors.KDTree(r_vectors; leafsize = leafsize)
+    
+    # Loop over bodies
+    bodies_indices = Random.shuffle(rng, Vector{Int64}(1:length(bodies)))
+    for i = 1 : length(bodies)
+      # Select body at random
+      bi = bodies_indices[i]
+      local b = bodies[bi]
+      
+      # Select its r_vectors
+      ri = r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1]
+      
+      # Find neighbors
+      indx = NearestNeighbors.inrange(kdtree, ri, d_tree)
+      
+      # Compute energy
+      e_current = external_enery(b, radius)
+      e_current += pairwise_energy_surface(ri, r_vectors, indx, e0, lambda)
+      
+      # Random step
+      dq = Random.randn(rng, Float64, 2) * dx
+      dorientation = Random.randn(rng, Float64) * dtheta
+      b.q += dq
+      b.orientation += dorientation
+      ri = body_module.get_r_vectors(b)
+      r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
+      
+      # Compute new energy
+      e_new = external_enery(b, radius)
+      e_new += pairwise_energy_surface(ri, r_vectors, indx, e0, lambda)
+      
+      # Do Metropolies step
+      mcmc = exp(-(e_new - e_current) / kT)
+      rn = rand(rng, Float64)
+      if rn < mcmc
+        b.q_old = copy(b.q)
+        b.orientation_old = copy(b.orientation)
+        r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
+      else
+        b.q = copy(b.q_old)
+        b.orientation = copy(b.orientation_old)
+        r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
+      end
+    end
+  end
+
+  # Save last config
+  if (n_steps % n_save == 0)
     write(f_config, string(length(bodies)), "\n")
     for b in bodies
       println(f_config, join(b.q, " "), " ", b.orientation)
     end
-  end
-
-  # Build tree
-  kdtree = NearestNeighbors.KDTree(r_vectors; leafsize = 64)
-
-  # Loop over bodies
-  bodies_indices = Random.shuffle(rng, Vector{Int64}(1:length(bodies)))
-  for i = 1 : length(bodies)
-    # Select body at random
-    bi = bodies_indices[i]
-    local b = bodies[bi]
-
-    # Select its r_vectors
-    ri = r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1]
-
-    # Find neighbors
-    indx = NearestNeighbors.inrange(kdtree, ri, d_tree)
-
-    # Compute energy
-    e_current = external_enery(b, radius)
-    e_current += pairwise_energy_surface(ri, r_vectors, indx, e0, lambda)
-    
-    # Random step
-    dq = Random.randn(rng, Float64, 2) * dx
-    dorientation = Random.randn(rng, Float64) * dtheta
-    b.q += dq
-    b.orientation += dorientation
-    ri = body_module.get_r_vectors(b)
-    r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
-
-    # Compute new energy
-    e_new = external_enery(b, radius)
-    e_new += pairwise_energy_surface(ri, r_vectors, indx, e0, lambda)
-  
-    # Do Metropolies step
-    mcmc = exp(-(e_new - e_current) / kT)
-    rn = rand(rng, Float64)
-    if rn < mcmc
-      b.q_old = copy(b.q)
-      b.orientation_old = copy(b.orientation)
-      r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
-    else
-      b.q = copy(b.q_old)
-      b.orientation = copy(b.orientation_old)
-      r_vectors[1:end, r_vectors_offsets[bi] : r_vectors_offsets[bi]+b.Nmarkers-1] = body_module.get_r_vectors(b)
-    end
-  end
+  end 
 end
 
-# Save last config
-if (n_steps % n_save == 0)
-  write(f_config, string(length(bodies)), "\n")
-  for b in bodies
-    println(f_config, join(b.q, " "), " ", b.orientation)
-  end
-end
+@time run(bodies, r_vectors, r_vectors_offsets, Nmarkers, n_steps, n_save, initial_step, kT, dx, dtheta, radius, e0, lambda)
+
 
 
